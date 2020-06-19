827726fcc4c808e95893314ca686fbcb49155173
dvdvgt <david.voigt1998@gmail.com>
2020-06-19 09:37:30 +0200

    Updated docs to exclude test javadoc annotations

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Vector2d"},{"p":"<Unnamed>","l":"Vector2dTest"},{"p":"main","l":"Vector3d"},{"p":"<Unnamed>","l":"Vector3dTest"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Vector2d"},{"p":"main","l":"Vector3d"}]
