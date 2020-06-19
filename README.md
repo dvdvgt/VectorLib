1e48548c5669192c40a9263df7bfa15c091cad9d
dvdvgt <david.voigt1998@gmail.com>
2020-06-19 13:56:38 +0200

    Updated README and added tests.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# VectorLib
-A simple 2d and 3d Vector Library for Java
+<h1 align="center">
+VectorLib
+</h1>
+
+## Table of content
+1. [Description](#description)
+2. [Documentation](#documentation)
+3. [Licence](#licence)
+
+## Description
+
+This is a simple library containing basic vector operations in either a R^2 or R^3 dimensional vector space.
+It may be useful when working with 2d or 3d vectors since all basic functionalities are already implemented.
+
+
+A compiled .jar file may be found [here](https://github.com/dvdvgt/VectorLib/releases)
+
+## Documentation
+
+Documentation can be found [here](https://dvdvgt.github.io/VectorLib).
+
+## Licence
+
+Copyright (c) 2020 David Voigt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- tests/Vector2dTest.java
+++ tests/Vector2dTest.java
@@ -2,8 +2,6 @@ import main.Vector2d;
 import org.junit.Test;
 import org.junit.Assert;
 
-import java.util.Vector;
-
 public class Vector2dTest {
 
     /**
@@ -13,21 +11,37 @@ public class Vector2dTest {
     public void testDist() {
         Vector2d v1 = new Vector2d(1, 2);
         Vector2d v2 = new Vector2d(3, 4);
-        Assert.assertEquals(2 * Math.sqrt(2), Vector2d.dist(v1, v2), 0.1);
+        Assert.assertEquals(2 * Math.sqrt(2), Vector2d.dist(v1, v2), 0.01);
 
-        Assert.assertEquals(v1.length(), Vector2d.dist(v1, new Vector2d(0, 0)), 0.1);
-        Assert.assertEquals(v1.length(), Vector2d.dist(new Vector2d(0, 0), v1), 0.1);
+        Assert.assertEquals(v1.length(), Vector2d.dist(v1, new Vector2d(0, 0)), 0.01);
+        Assert.assertEquals(v1.length(), Vector2d.dist(new Vector2d(0, 0), v1), 0.01);
+
+        v1.set(-12, -5);
+        v2.set(-789, -5);
+        Assert.assertEquals(777, Vector2d.dist(v1, v2), 0.01);
     }
 
+    /**
+     * Tests for Vector2d.normalize
+     */
     @Test
     public void testNormalize() {
         Vector2d v = new Vector2d(5, 5);
         Vector2d ret = new Vector2d(0, -25);
         Vector2d.normalize(v, ret);
-        Assert.assertEquals(1.0, ret.length(), 0.1);
-        Assert.assertEquals(1.0, Vector2d.normalize(v).length(), 0.1);
+        Assert.assertEquals(1.0, ret.length(), 0.10);
+
+        Assert.assertEquals(1.0, Vector2d.normalize(v).length(), 0.01);
+
         v.normalize();
-        Assert.assertEquals(1.0, v.length(), 0.1);
+        Assert.assertEquals(1.0, v.length(), 0.01);
+    }
+
+    @Test
+    public void testLength() {
+        Vector2d v = new Vector2d();
+        Assert.assertEquals(0.0, v.length(), 0.01);
+        Assert.assertEquals(0.0, Vector2d.length(v), 0.01);
     }
 
     /**
